ca19cc7474a59c3ea75aac83e019d7e04fcd3012
g3tbusy <duehe62bs3@gmail.com>
2024-03-27 19:52:17 +0300

    commit

--- sql queries/routines.sql
+++ sql queries/routines.sql
@@ -1,50 +1,62 @@
--- Процедура для обновления названия категории в таблице Products
-CREATE OR REPLACE PROCEDURE update_product_category_name(
-    IN old_category_name VARCHAR(50),
-    IN new_category_name VARCHAR(50)
+-- Процедура для добавления нового пользователя
+CREATE OR REPLACE PROCEDURE add_new_user(
+    p_user_name VARCHAR(20),
+    p_user_email VARCHAR(50),
+    p_user_password CHAR(50),
+    p_user_mobile_number VARCHAR(11)
 )
     LANGUAGE plpgsql
 AS
 $$
 BEGIN
-    UPDATE Products
-    SET category_name = new_category_name
-    WHERE category_name = old_category_name;
+    INSERT INTO Users (user_name, user_email, user_password, user_mobile_number)
+    VALUES (p_user_name, p_user_email, p_user_password, p_user_mobile_number);
 END;
 $$;
 
-
--- Процедура для обновления названия категории в таблице Orders
-CREATE OR REPLACE PROCEDURE update_order_category_name(
-    IN old_category_name VARCHAR(50),
-    IN new_category_name VARCHAR(50)
-)
-    LANGUAGE plpgsql
-AS
-$$
-    BEGIN
-UPDATE Orders
-SET product_name = new_category_name
-WHERE product_id IN (SELECT product_id FROM Products WHERE category_name = old_category_name);
-END;
-$$;
+CALL add_new_user('new user', 'email@example.com', 'password123', '12345678901');
 
 
--- Процедура для обновления названия категории в таблице ProductReviews
-CREATE OR REPLACE PROCEDURE update_productreviews_category_name(
-    IN old_category_name VARCHAR(50),
-    IN new_category_name VARCHAR(50)
+-- Процедура для добавления нового товара
+CREATE OR REPLACE PROCEDURE add_new_product(
+    p_product_name VARCHAR(100),
+    p_product_model VARCHAR(60),
+    p_product_description VARCHAR(300),
+    p_product_price NUMERIC(7, 2),
+    p_category_name VARCHAR(50),
+    p_category_id INT
 )
     LANGUAGE plpgsql
 AS
 $$
 BEGIN
-    UPDATE ProductReviews
-    SET product_id = (SELECT product_id
-                      FROM Products
-                      WHERE category_name = new_category_name)
-    WHERE product_id IN (SELECT product_id
-                         FROM Products
-                         WHERE category_name = old_category_name);
+    INSERT INTO Products (product_name, product_model, product_description, product_price, category_name, category_id)
+    VALUES (p_product_name, p_product_model, p_product_description, p_product_price, p_category_name, p_category_id);
 END;
 $$;
+
+CALL add_new_product('new product', 'model X', 'description', 1000.00, 'Старая категория', 30);
+
+
+-- Процедура для добавления нового заказа
+CREATE OR REPLACE PROCEDURE add_new_order(
+    p_user_id INT,
+    p_order_status VARCHAR(30),
+    p_product_id INT,
+    p_product_name VARCHAR(100),
+    p_delivery_address VARCHAR(100),
+    p_order_price NUMERIC(7, 2),
+    p_order_delivery_price INT
+)
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    INSERT INTO Orders (user_id, order_status, product_id, product_name, delivery_address, order_price,
+                        order_delivery_price)
+    VALUES (p_user_id, p_order_status, p_product_id, p_product_name, p_delivery_address, p_order_price,
+            p_order_delivery_price);
+END;
+$$;
+
+CALL add_new_order(5, 'В обработке', 103, 'new product', 'new address', 1000.00, 50.00);
