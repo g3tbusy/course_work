3d2a99f7d40cb72e816e7d8063fc5d83151cf2c9
g3tbusy <duehe62bs3@gmail.com>
2024-03-28 19:42:32 +0300

    commit

---
+++ sql queries/roles.sql
@@ -0,0 +1,19 @@
+-- Роль админа
+CREATE ROLE admin WITH LOGIN PASSWORD 'admin';
+GRANT ALL PRIVILEGES ON Users, Categories, Products, Orders, ProductReviews TO admin;
+
+-- Роль стаффа
+CREATE ROLE moderator;
+GRANT SELECT, INSERT, UPDATE (user_name, user_email, user_mobile_number) ON Users TO moderator;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Orders TO moderator;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Products TO moderator;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ProductReviews TO moderator;
+
+-- Роль клиента
+CREATE ROLE customer;
+GRANT SELECT, INSERT, UPDATE ON Users TO customer;
+GRANT SELECT, INSERT, UPDATE ON Orders TO customer;
+GRANT SELECT, INSERT ON ProductReviews TO customer;
+GRANT SELECT ON Products TO customer;
+GRANT SELECT ON Categories TO customer;
+
