d4af7f9b0ea5f5486a0731cc837a3887714e45f5
g3tbusy <duehe62bs3@gmail.com>
2024-03-28 03:20:22 +0300

    commit

--- sql queries/routines.sql
+++ sql queries/routines.sql
@@ -1,62 +1,128 @@
--- Процедура для добавления нового пользователя
-CREATE OR REPLACE PROCEDURE add_new_user(
-    p_user_name VARCHAR(20),
-    p_user_email VARCHAR(50),
-    p_user_password CHAR(50),
-    p_user_mobile_number VARCHAR(11)
+-- -- -- -- -- Процедуры на каскадное обновление -- -- -- -- --
+-- 1. Процедура «update_product_price» обновляет цену продукта в таблице Products и все связанные данные в таблице Orders.
+CREATE PROCEDURE update_product_price(
+    IN p_product_id INT,
+    IN p_new_price NUMERIC(7, 2)
 )
     LANGUAGE plpgsql
 AS
 $$
 BEGIN
-    INSERT INTO Users (user_name, user_email, user_password, user_mobile_number)
-    VALUES (p_user_name, p_user_email, p_user_password, p_user_mobile_number);
+    UPDATE Products
+    SET product_price = p_new_price
+    WHERE product_id = p_product_id;
+
+    UPDATE Orders
+    SET order_price = p_new_price
+    WHERE product_id = p_product_id;
 END;
 $$;
+CALL update_product_price(1, 999.99);
 
-CALL add_new_user('new user', 'email@example.com', 'password123', '12345678901');
-
--- Процедура для добавления нового товара
-CREATE OR REPLACE PROCEDURE add_new_product(
-    p_product_name VARCHAR(100),
-    p_product_model VARCHAR(60),
-    p_product_description VARCHAR(300),
-    p_product_price NUMERIC(7, 2),
-    p_category_name VARCHAR(50),
-    p_category_id INT
+-- 2. Каскадная процедура на обновление данных для таблицы Products, которая также обновляет связанные данные в таблице Orders.
+CREATE PROCEDURE update_product_and_orders(
+    IN p_product_id INT,
+    IN p_product_name VARCHAR(100),
+    IN p_product_model VARCHAR(60),
+    IN p_product_description VARCHAR(300),
+    IN p_product_price NUMERIC(7, 2)
 )
     LANGUAGE plpgsql
 AS
 $$
 BEGIN
-    INSERT INTO Products (product_name, product_model, product_description, product_price, category_name, category_id)
-    VALUES (p_product_name, p_product_model, p_product_description, p_product_price, p_category_name, p_category_id);
+    UPDATE Products
+    SET product_name        = p_product_name,
+        product_model       = p_product_model,
+        product_description = p_product_description,
+        product_price       = p_product_price
+    WHERE product_id = p_product_id;
+    UPDATE Orders
+    SET product_name = p_product_name
+    WHERE product_id = p_product_id;
 END;
 $$;
+CALL update_product_and_orders(102, 'New Product Name', 'New Model', 'New Description', 99.99);
 
-CALL add_new_product('new product', 'model X', 'description', 1000.00, 'Холодильники', 1);
-
-
--- Процедура для добавления нового заказа
-CREATE OR REPLACE PROCEDURE add_new_order(
-    p_order_id INT,
+-- 3. Каскадная процедура на обновление данных для таблицы Users, которая также обновляет связанные данные в таблице Orders.
+CREATE PROCEDURE update_user_and_orders(
     p_user_id INT,
-    p_order_status VARCHAR(30),
-    p_product_id INT,
-    p_product_name VARCHAR(100),
-    p_delivery_address VARCHAR(100),
-    p_order_price NUMERIC(7, 2),
-    p_order_delivery_price INT
+    p_user_name VARCHAR,
+    p_user_email VARCHAR,
+    p_user_password CHAR(50),
+    p_user_mobile_number VARCHAR,
+    p_delivery_address VARCHAR
 )
     LANGUAGE plpgsql
 AS
 $$
 BEGIN
-    INSERT INTO Orders (order_id, user_id, order_status, product_id, product_name, delivery_address, order_price,
-                        order_delivery_price)
-    VALUES (p_order_id, p_user_id, p_order_status, p_product_id, p_product_name, p_delivery_address, p_order_price,
-            p_order_delivery_price);
+    UPDATE Users
+    SET user_name          = p_user_name,
+        user_email         = p_user_email,
+        user_password      = p_user_password,
+        user_mobile_number = p_user_mobile_number
+    WHERE user_id = p_user_id;
+    UPDATE Orders
+    SET delivery_address = p_delivery_address
+    WHERE user_id = p_user_id;
 END;
 $$;
 
-CALL add_new_order(112,5, 'В обработке', 103, 'new product', 'new address', 1000.00, 50);
+CALL update_user_and_orders(89, 'newname', 'newemail2@example.com', 'newpassword', '79749766551','new delivery address');
+
+
+-- -- -- -- -- Процедуры на каскадное удаление -- -- -- -- --
+-- 1. Процедура «Delete_User» удаляет пользователя из таблицы Users и все связанные с ним заказы и отзывы о продуктах.
+CREATE PROCEDURE Delete_User(
+    IN p_user_id INT
+)
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    DELETE
+    FROM ProductReviews
+    WHERE user_id = p_user_id;
+    DELETE
+    FROM Orders
+    WHERE user_id = p_user_id;
+    DELETE
+    FROM Users
+    WHERE user_id = p_user_id;
+END;
+$$;
+
+CALL Delete_User(11);
+
+-- 2. Процедура предназначена для удаления продукта и связанных с ним отзывов и заказов из базы данных.
+CREATE PROCEDURE delete_product_and_reviews(p_product_id INT)
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    DELETE FROM Orders WHERE product_id = p_product_id;
+    DELETE FROM ProductReviews WHERE product_id = p_product_id;
+    DELETE FROM Products WHERE product_id = p_product_id;
+END;
+$$;
+
+CALL delete_product_and_reviews(89);
+
+-- 3.
+CREATE PROCEDURE delete_category_products_orders_categories_reviews(IN category_id_to_delete INT)
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    product_ids INT[];
+BEGIN
+    SELECT array_agg(product_id) INTO product_ids FROM Products WHERE category_id = category_id_to_delete;
+    DELETE FROM ProductReviews WHERE product_id = ANY (product_ids);
+    DELETE FROM Orders WHERE product_id = ANY (product_ids);
+    DELETE FROM Products WHERE category_id = category_id_to_delete;
+    DELETE FROM Categories WHERE category_id = category_id_to_delete;
+END;
+$$;
+
+CALL delete_category_products_orders_categories_reviews(1);
